ccca30f692e4c28dfb2a162f1bad15827356e104
hqlinh99 <hqlinh040599@gmail.com>
2023-11-07 17:11:44 +0700

    v3.0

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,4 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hêloooo");
     }
-
-    public static void main(String[] args) {
-        System.out.println("Hêloooo");
-    }
-
-
 }
---
+++ src/main/java/Nguyen.java
@@ -0,0 +1,5 @@
+public class Nguyen {
+    public static void main(String[] args) {
+        System.out.println("Chinh sua di");
+    }
+}
