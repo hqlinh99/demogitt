d0457dda2c8bb9e4680f8fa5a317e40c4080b782
thainguyen004 <nguyennt11032004@gmail.com>
2023-11-07 17:15:15 +0700

    nguyen-push

--- src/main/java/Nguyen.java
+++ src/main/java/Nguyen.java
@@ -1,5 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Nguyen {
     public static void main(String[] args) {
         System.out.println("Chinh sua di");
+        List<String> list=  new ArrayList<>();
     }
 }
