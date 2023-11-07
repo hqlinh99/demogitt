23e0c588bb7b117891584d849ab0a692a8df104b
thainguyen004 <nguyennt11032004@gmail.com>
2023-11-07 17:47:06 +0700

    adafajfo

--- src/main/java/Nguyen.java
+++ src/main/java/Nguyen.java
@@ -5,5 +5,6 @@ public class Nguyen {
     public static void main(String[] args) {
         System.out.println("Chinh sua di");
         List<String> list=  new ArrayList<>();
+        List<String> list=  new ArrayList<>();
     }
 }
